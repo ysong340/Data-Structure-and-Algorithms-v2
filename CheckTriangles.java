349a897a98c4ac3b820a5edc73e06c1e2845f019
Yang Song <songyangustc@gmail.com>
2017-03-10 05:49:09 -0800

    Update CheckTriangles.java

--- CheckTriangles.java
+++ CheckTriangles.java
@@ -14,4 +14,35 @@ public CheckTriangles {
         }
         return false;
     }
+ 
+    public List<List<Integer>> getAllTriangles(int[] nums) {
+        Arrays.sort(nums);
+     
+        List<List<Integer>> results = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length - 2) {
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] <= nums[k]) {
+                    k--;
+                } else {
+                    generateTriangles(nums, i, j, k, results);
+                }
+                j++;
+                while (j < nums.length - 1 && nums[j] == nums[j - 1]) j++;
+                k = nums.length - 1;
+            }
+            i++;
+            while (i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
+        }
+        return results;
+    }
+ 
+    private void generateTriangles(int[] nums, int i, int j, int k, List<List<Integer>> results) {
+        while (j < k) {
+            results.add(Arrays.asList(nums[i], nums[j], nums[k]));
+            k--;
+            while (k > j && nums[k] == nums[k + 1]) k--;
+        }
+    }
 }
